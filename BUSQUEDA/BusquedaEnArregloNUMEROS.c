38661ca16f654e234e494f5d23b2c7f09d7fdda8
danesabenitez <danesabenitez7@gmail.com>
2026-05-29 01:35:12 -0300

    tuturu

---
+++ BUSQUEDA/BusquedaEnArregloNUMEROS.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    // rellenado del array
+    int numeros[5];
+    for (int i = 1; i < 6; i++)
+    {
+        printf("Ingrese un número: ");
+        scanf("%d", &numeros[i]);
+    }
+    // pedido de búsqueda al usurio
+    printf("Ingrese el número a buscar: ");
+    int buscado = 0;
+    scanf("%d", &buscado);
+
+    // búsqueda del número
+    int encontrado = 0;
+    for (int i = 1; i < 6; i++)
+    {
+        if (numeros[i] == buscado)
+        {
+            printf("Encontrado en la posición %d\n", i);
+            encontrado = 1;
+            break;
+        }
+    }
+    if (!encontrado)
+    {
+        printf("No encontrado.\n");
+    }
+
+    return 0;
+}
